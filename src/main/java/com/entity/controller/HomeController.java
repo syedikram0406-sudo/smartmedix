4429249acc8d8b02570c3c5de33048a0342f8de0
syed ikram <syedikram0406@gmail.com>
2026-06-24 13:42:34 +0530

    deploy

--- src/main/java/com/entity/controller/HomeController.java
+++ src/main/java/com/entity/controller/HomeController.java
@@ -2,12 +2,24 @@ package com.entity.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
-    @GetMapping({"/", "/home"})
+    @GetMapping("/")
+    public String root() {
+        return "home";
+    }
+
+    @GetMapping("/home")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "WORKING";
     }
 }
